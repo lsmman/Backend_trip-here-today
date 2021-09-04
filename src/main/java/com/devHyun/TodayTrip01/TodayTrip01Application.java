21582a58710b606c2f359932185f2dfa02923d24
t1dmlgus <dmlgusgngl@gmail.com>
2021-09-04 12:40:54 +0900

    백엔드 초기세팅

---
+++ src/main/java/com/devHyun/TodayTrip01/TodayTrip01Application.java
@@ -0,0 +1,13 @@
+package com.devHyun.TodayTrip01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayTrip01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayTrip01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devHyun/TodayTrip01/TodayTrip01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devHyun.TodayTrip01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayTrip01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
